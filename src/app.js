581927b0e6836f7766d7b84a3a3331c9594b77ff
Daazd <zoomie067@yahoo.com>
2024-03-27 11:18:30 -0700

    activate changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (req, res) => {
     try {
@@ -13,6 +16,51 @@ app.get("/restaurants", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+app.get("/restaurants/:id", async (req, res) => {
+    try {
+        const restaurant = await Restaurant.findByPk(req.params.id);
+        if (restaurant) {
+            res.json(restaurant);
+        } else {
+            res.status(404).json({ error: "Restaurant not found" });
+        }
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+    try {
+        const restaurant = await Restaurant.findByPk(req.params.id);
+        if (restaurant) {
+            await restaurant.update(req.body);
+            res.json(restaurant);
+        } else {
+            res.status(404).json({ error: "Restaurant not found" });
+        }
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+    try {
+        const restaurant = await Restaurant.findByPk(req.params.id);
+        if (restaurant) {
+            await restaurant.destroy();
+            res.status(204).end();
+        } else {
+            res.status(404).json({ error: "Restaurant not found" });
+        }
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 
 
